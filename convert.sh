963c2e2d3d1bffd45526a3292552feea6766b979
clementGilardy <clement.gilardy@outlook.fr>
2015-04-30 10:34:00 +0200

    add convert

--- convert.sh
+++ convert.sh
@@ -4,10 +4,7 @@ if [ $# -gt 1  ] ; then
     if [[ $1 == "-mp3" ]] ; then
         for t in "${@:2}"
         do
-            echo ${t}
-            echo "Converting ${t} in ${t%.wav}"
             faad "${t}"
-            echo "Converting ${t%.wav} in ${t%.mp3}"
             lame "${t%.m4a}.wav" "${t%.m4a}.mp3"
             rm "${t%.m4a}.wav"
             rm "${t}"
@@ -17,7 +14,6 @@ if [ $# -gt 1  ] ; then
     if [[ $1 == "-wav" ]] ; then
         for t in "${@:2}"
         do
-            echo "Converting ${t} in ${t%.wav}"
             faad "${t}"
             rm "${t}"
         done
